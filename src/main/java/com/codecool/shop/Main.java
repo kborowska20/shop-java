e620b1c0de8abbfd9ab9784aedb99c5295641ef8
Ickerday <elitarform@gmail.com>
2017-04-25 23:21:07 +0200

    Add some test method invoking to Main

--- src/main/java/com/codecool/shop/Main.java
+++ src/main/java/com/codecool/shop/Main.java
@@ -1,9 +1,22 @@
 package com.codecool.shop;
 
-import com.codecool.shop.dao.DbConnector;
+import com.codecool.shop.dao.implementation.ProductDaoSQLite;
+import com.codecool.shop.model.Product;
+import com.codecool.shop.model.ProductCategory;
+import com.codecool.shop.model.Supplier;
+
 
 public class Main {
     public static void main(String args[]) {
-        new DbConnector();
+        ProductDaoSQLite dbConn = new ProductDaoSQLite();
+        for (Product product : dbConn.getAll()) {
+            System.out.println(product.toString());
+        }
+
+        dbConn.add(new Product("Name", 3f,
+                "PLN", "Desc",
+                new ProductCategory("Test", "Test", "Test"),
+                new Supplier("Test", "Test")));
+
     }
 }
