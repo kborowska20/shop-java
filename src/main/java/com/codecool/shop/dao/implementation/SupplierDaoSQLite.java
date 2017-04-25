5f64b1f2ba672c8452917f8c9d7776c5ac49b172
Ickerday <elitarform@gmail.com>
2017-04-25 22:27:57 +0200

    Add empty methods to SupplierDaoSQLite

--- src/main/java/com/codecool/shop/dao/implementation/SupplierDaoSQLite.java
+++ src/main/java/com/codecool/shop/dao/implementation/SupplierDaoSQLite.java
@@ -1,4 +1,29 @@
 package com.codecool.shop.dao.implementation;
 
-public class SupplierDaoSQLite  {
+import com.codecool.shop.dao.SupplierDao;
+import com.codecool.shop.model.Supplier;
+
+import java.util.List;
+
+public class SupplierDaoSQLite implements SupplierDao {
+
+    @Override
+    public void add(Supplier supplier) {
+
+    }
+
+    @Override
+    public Supplier find(int id) {
+        return null;
+    }
+
+    @Override
+    public void remove(int id) {
+
+    }
+
+    @Override
+    public List<Supplier> getAll() {
+        return null;
+    }
 }
