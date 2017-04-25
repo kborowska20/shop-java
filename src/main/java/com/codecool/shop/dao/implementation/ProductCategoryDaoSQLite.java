2e55b005ca1186c041feec6917eea462b2c8b5a9
Ickerday <elitarform@gmail.com>
2017-04-25 22:31:26 +0200

    Add empty methods to ProductCategoryDaoSQLIte

--- src/main/java/com/codecool/shop/dao/implementation/ProductCategoryDaoSQLite.java
+++ src/main/java/com/codecool/shop/dao/implementation/ProductCategoryDaoSQLite.java
@@ -1,4 +1,28 @@
 package com.codecool.shop.dao.implementation;
 
-public class ProductCategoryDaoSQLite {
+import com.codecool.shop.dao.ProductCategoryDao;
+import com.codecool.shop.model.ProductCategory;
+
+import java.util.List;
+
+public class ProductCategoryDaoSQLite implements ProductCategoryDao {
+
+    @Override
+    public void add(ProductCategory category) {
+
+    }
+
+    @Override
+    public ProductCategory find(int id) {
+        return null;
+    }
+
+    @Override
+    public void remove(int id) {
+    }
+
+    @Override
+    public List<ProductCategory> getAll() {
+        return null;
+    }
 }
