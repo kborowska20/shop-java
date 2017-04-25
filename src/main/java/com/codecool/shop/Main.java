5425582701d5073ee192d286df4b82d5239731cd
Ickerday <elitarform@gmail.com>
2017-04-25 18:36:13 +0200

    Restore main

--- src/main/java/com/codecool/shop/Main.java
+++ src/main/java/com/codecool/shop/Main.java
@@ -3,7 +3,7 @@ package com.codecool.shop;
 import com.codecool.shop.dao.DbConnector;
 
 public class Main {
-    public static void main( String args[] ) {
+    public static void main(String args[]) {
         new DbConnector();
     }
 }
