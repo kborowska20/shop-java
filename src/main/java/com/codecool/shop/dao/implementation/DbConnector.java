b4d3716d857e8cf082054d7f34b175a47c8ac3b7
Ickerday <elitarform@gmail.com>
2017-04-25 23:19:32 +0200

    Partial fix to insert() in DbConnector

--- src/main/java/com/codecool/shop/dao/implementation/DbConnector.java
+++ src/main/java/com/codecool/shop/dao/implementation/DbConnector.java
@@ -6,7 +6,6 @@ import com.codecool.shop.model.Supplier;
 import com.sun.corba.se.impl.io.TypeMismatchException;
 
 import java.sql.*;
-import java.text.MessageFormat;
 import java.util.List;
 
 class DbConnector<T> {
@@ -27,29 +26,31 @@ class DbConnector<T> {
         conn.close();
     }
 
-    public void insert(T t) throws SQLException {
+
+    void insert(T t) throws SQLException {
         try {
             Connection conn = this.connect();
             Statement dbStatement = conn.createStatement();
 
-
             String query = "INSERT OR REPLACE INTO ";
             if (t instanceof Product) {
-                query += MessageFormat.format("product (id, name, defaultPrice, currencyString, description, " +
-                                "categoryID, supplierID) VALUES ((SELECT id FROM product WHERE " +
-                                "id={1}), {2}, {3}, {4}, {5}, {6}, {7});",
-                        ((Product) t).getId(), ((Product) t).getName(), ((Product) t).getDefaultPrice(),
-                        ((Product) t).getDescription(), ((Product) t).getProductCategory().getId(),
-                        ((Product) t).getSupplier().getId());
+                query += "product (id, name, defaultPrice, currencyString, description, categoryID, supplierID)"
+                        + " VALUES ((SELECT id FROM product WHERE id=" + ((Product) t).getId() + "), '" +
+                        ((Product) t).getName() + "', " +
+                        ((Product) t).getDefaultPrice() +
+                        ", 'PLN', '" +
+                        ((Product) t).getDescription() +
+                        "', 7, 8);";
+
             } else if (t instanceof ProductCategory) {
-                query += MessageFormat.format("productCategory (id, name, department, description) " +
-                                "VALUES ((SELECT id FROM product WHERE id={1}), {2}, {3}, {4});",
-                        ((ProductCategory) t).getId(), ((ProductCategory) t).getName(),
-                        ((ProductCategory) t).getDepartment(), ((ProductCategory) t).getDescription());
+//                query += MessageFormat.format("productCategory (id, name, department, description) " +
+//                                "VALUES ((SELECT id FROM product WHERE id={1}), \'{2}\', \'{3}\', \'{4}\');",
+//                        ((ProductCategory) t).getId(), ((ProductCategory) t).getName(),
+//                        ((ProductCategory) t).getDepartment(), ((ProductCategory) t).getDescription());
             } else if (t instanceof Supplier) {
-                query += MessageFormat.format("supplier (id, name, description) " +
-                                "VALUES ((SELECT id FROM product WHERE id={1}), {2}, {3});",
-                        ((Supplier) t).getId(), ((Supplier) t).getName(), ((Supplier) t).getDescription());
+//                query += MessageFormat.format("supplier (id, name, description) " +
+//                                "VALUES ((SELECT id FROM product WHERE id={1}), \'{2}\', \'{3}\');",
+//                        ((Supplier) t).getId(), ((Supplier) t).getName(), ((Supplier) t).getDescription());
             } else {
                 throw new TypeMismatchException("Unsupported type of the object provided!");
             }
@@ -57,16 +58,23 @@ class DbConnector<T> {
             System.out.println(query);
             dbStatement.execute(query);
             dbStatement.close();
-            conn.commit();
             this.closeConnection(conn);
 
+
         } catch (Exception e) {
+            e.printStackTrace();
             throw new SQLException("Couldn't upsert to database!");
         }
     }
 
-    public void insert(List<T> insertList) {
-
+    void insert(List<T> insertList) {
+        try {
+            for (T insertedElement : insertList) {
+                this.insert(insertedElement);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
 }
