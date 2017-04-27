ff3fac31ec64d87b7d847417246993a223c05c82
Ickerday <elitarform@gmail.com>
2017-04-27 11:01:12 +0200

    Add showAllProducts, showBy, addProduct, removeProduct to ProductController

--- src/main/java/com/codecool/shop/controller/ProductController.java
+++ src/main/java/com/codecool/shop/controller/ProductController.java
@@ -1,13 +1,26 @@
 package com.codecool.shop.controller;
 
-import com.codecool.shop.dao.ProductCategoryDao;
-import com.codecool.shop.dao.ProductDao;
-
-
-import java.util.HashMap;
-import java.util.Map;
+import com.codecool.shop.dao.implementation.ProductDaoSQLite;
+import com.codecool.shop.model.Product;
+import com.codecool.shop.view.ProductView;
 
 public class ProductController {
+    public static ProductDaoSQLite productDao;
 
+    public static void showAllProducts() {
+        ProductView.printProductList(productDao.getAll());
+    }
+
+    public static void showProductBy(Integer id) {
+        ProductView.printProduct(productDao.find(id));
+    }
+
+    public static void addProduct(Product product) {
+        productDao.add(product);
+    }
+
+    public static void removeProduct(Integer id) {
+        productDao.remove(id);
+    }
 
 }
