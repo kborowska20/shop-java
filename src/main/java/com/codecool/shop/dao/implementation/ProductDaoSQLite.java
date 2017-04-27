787d522e32f5f3409076b80c3a20c5f16086ec49
Ickerday <elitarform@gmail.com>
2017-04-27 12:24:36 +0200

    Make getBy() methods use filterProductsBy from DbConnector

--- src/main/java/com/codecool/shop/dao/implementation/ProductDaoSQLite.java
+++ src/main/java/com/codecool/shop/dao/implementation/ProductDaoSQLite.java
@@ -90,57 +90,13 @@ public class ProductDaoSQLite implements ProductDao {
 
     @Override
     public List<Product> getBy(Supplier supplier) {
-        List<Product> productList = new ArrayList<>();
-        try {
-            DbConnector dbConn = new DbConnector();
-            Connection conn = dbConn.connect();
-            Statement dbStatement = conn.createStatement();
-            // TODO
-            ResultSet resultSet = dbStatement.executeQuery("SELECT name, defaultPrice, " +
-                    "currencyString, description FROM product;");
-
-            while (resultSet.next()) {
-                productList.add(new Product(resultSet.getString("name"),
-                        resultSet.getFloat("defaultPrice"),
-                        resultSet.getString("currencyString"),
-                        resultSet.getString("description"),
-                        new ProductCategory("Test", "Test", "Test"),
-                        supplier));
-            }
-
-            dbStatement.close();
-            dbConn.closeConnection(conn);
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-        return productList;
+        DbConnector<Supplier> dbConn = new DbConnector<>();
+        return dbConn.filterProductsBy(supplier);
     }
 
     @Override
-    public List<Product> getBy(ProductCategory productCategory) {
-        List<Product> productList = new ArrayList<>();
-        try {
-            DbConnector dbConn = new DbConnector();
-            Connection conn = dbConn.connect();
-            Statement dbStatement = conn.createStatement();
-            // TODO
-            ResultSet resultSet = dbStatement.executeQuery("SELECT name, defaultPrice, " +
-                    "currencyString, description FROM product;");
-
-            while (resultSet.next()) {
-                productList.add(new Product(resultSet.getString("name"),
-                        resultSet.getFloat("defaultPrice"),
-                        resultSet.getString("currencyString"),
-                        resultSet.getString("description"),
-                        productCategory,
-                        new Supplier("Test", "Test")));
-            }
-
-            dbStatement.close();
-            dbConn.closeConnection(conn);
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-        return productList;
+    public List<Product> getBy(ProductCategory category) {
+        DbConnector<ProductCategory> dbConn = new DbConnector<>();
+        return dbConn.filterProductsBy(category);
     }
 }
