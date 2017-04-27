3c0b3567d7e1d8f1edf11134fecc16b19c18f6c8
Ickerday <elitarform@gmail.com>
2017-04-27 19:16:02 +0200

    Add getMenu, showMessage in MenuController

--- src/main/java/com/codecool/shop/controller/MenuController.java
+++ src/main/java/com/codecool/shop/controller/MenuController.java
@@ -3,4 +3,11 @@ import com.codecool.shop.view.MenuView;
 
 public class MenuController {
 
+    public static void getMenu() {
+        MenuView.viewMainMenu();
+    }
+
+    public static void showMessage(String msg) {
+        MenuView.viewMainMenu();
+    }
 }
