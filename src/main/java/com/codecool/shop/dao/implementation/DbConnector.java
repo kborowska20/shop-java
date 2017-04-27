5052ae813e467311cd9042d116502029dfb45ab6
Ickerday <elitarform@gmail.com>
2017-04-27 14:45:54 +0200

    Add delete(), getAllProducts() to DbConnector

--- src/main/java/com/codecool/shop/dao/implementation/DbConnector.java
+++ src/main/java/com/codecool/shop/dao/implementation/DbConnector.java
@@ -27,7 +27,6 @@ class DbConnector<T> {
         conn.close();
     }
 
-
     void insert(T t) throws SQLException {
         try {
             Connection conn = this.connect();
@@ -83,18 +82,52 @@ class DbConnector<T> {
         }
     }
 
-    List<Product> getAllProducts() throws TypeMismatchException {
-        return filterProductsBy(null);
+    void delete(T t) throws SQLException {
+        Integer itemId = null;
+        String itemClassName = null;
+        System.out.println(t.getClass().getName().toLowerCase());
+        try {
+            Connection conn = this.connect();
+            Statement dbStatement = conn.createStatement();
+
+            if (t instanceof Product) {
+                itemId = ((Product) t).getId();
+                itemClassName = "product";
+            } else if (t instanceof ProductCategory) {
+                itemId = ((ProductCategory) t).getId();
+                itemClassName = "productCategory";
+            } else if (t instanceof Supplier) {
+                itemId = ((Supplier) t).getId();
+                itemClassName = "supplier";
+            }
+
+            String query = "DELETE FROM " + itemClassName + " WHERE id=" + itemId;
+
+            dbStatement.execute(query);
+            dbStatement.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+            throw new SQLException("Couldn't delete from database!");
+        }
     }
 
-    List<Product> filterProductsBy(T t) throws TypeMismatchException {
+    List<Product> getAllProducts() {
+        List<Product> productList = new ArrayList<>();
+        try {
+            productList = filterProductsBy(null);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return productList;
+    }
+
+    List<Product> filterProductsBy(T t) throws SQLException {
         List<Product> productList = new ArrayList<>();
         Supplier supplier = new Supplier(null, null);
         ProductCategory category = new ProductCategory(null, null, null);
 
         try {
-            DbConnector dbConn = new DbConnector();
-            Connection conn = dbConn.connect();
+            Connection conn = this.connect();
             Statement dbStatement = conn.createStatement();
             ResultSet resultSet = dbStatement.executeQuery("SELECT * FROM product;");
 
@@ -112,12 +145,61 @@ class DbConnector<T> {
                         category, supplier));
             }
 
+            resultSet.close();
             dbStatement.close();
-            dbConn.closeConnection(conn);
+            closeConnection(conn);
         } catch (SQLException e) {
             e.printStackTrace();
+            throw new SQLException("Couldn't delete from database!");
+
         }
         return productList;
+    }
+
+    List<ProductCategory> getAllCategories() throws SQLException {
+        List<ProductCategory> categoryList = new ArrayList<>();
+
+        try {
+            Connection conn = this.connect();
+            Statement dbStatement = conn.createStatement();
+            ResultSet resultSet = dbStatement.executeQuery("SELECT * FROM productCategory;");
+
+            while (resultSet.next()) {
+                categoryList.add(new ProductCategory(resultSet.getString("name"),
+                        resultSet.getString("department"),
+                        resultSet.getString("description")));
+            }
+
+            resultSet.close();
+            dbStatement.close();
+            conn.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+            throw new SQLException("Couldn't get categories from database!");
+
+        }
+        return categoryList;
+
+    }
+
+    List<Supplier> getAllSuppliers() throws SQLException {
+        List<Supplier> supplierList = new ArrayList<>();
+
+        try {
+            Connection conn = this.connect();
+            Statement dbStatement = conn.createStatement();
+            ResultSet resultSet = dbStatement.executeQuery("SELECT * FROM supplier;");
+
+            while (resultSet.next()) {
+                supplierList.add(new Supplier(resultSet.getString("name"),
+                        resultSet.getString("description")));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+            throw new SQLException("Couldn't get suppliers from database!");
+
+        }
+        return supplierList;
     }
 
 }
