71ed328cc2a607ba7e845067e1ee92724e9ca4c0
Ickerday <elitarform@gmail.com>
2017-04-27 13:28:50 +0200

    Fix NullPointerException in ProductController, started getProductsBy

--- src/main/java/com/codecool/shop/controller/ProductController.java
+++ src/main/java/com/codecool/shop/controller/ProductController.java
@@ -2,17 +2,22 @@ package com.codecool.shop.controller;
 
 import com.codecool.shop.dao.implementation.ProductDaoSQLite;
 import com.codecool.shop.model.Product;
+import com.codecool.shop.model.ProductCategory;
 import com.codecool.shop.view.ProductView;
 
 public class ProductController {
-    private static ProductDaoSQLite productDao;
+    private static ProductDaoSQLite productDao = new ProductDaoSQLite();
 
     public static void showAllProducts() {
         ProductView.printProductList(productDao.getAll());
     }
 
-    public static void showProductBy(Integer id) {
+    public static void findProductBy(Integer id) {
         ProductView.printProduct(productDao.find(id));
+    }
+
+    public static void getProductsBy(ProductCategory category) {
+
     }
 
     public static void addProduct(Product product) {
