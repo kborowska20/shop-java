bcadd56dad7242f7327b68de621d4a0597b83777
Ickerday <elitarform@gmail.com>
2017-05-08 15:26:50 +0200

    Make Controller initializers local

--- src/main/java/com/codecool/shop/ShopApp.java
+++ src/main/java/com/codecool/shop/ShopApp.java
@@ -8,35 +8,41 @@ import spark.template.thymeleaf.ThymeleafTemplateEngine;
 
 class ShopApp {
 
-    private ProductController productController = new ProductController();
-    private ProductCategoryController categoryController = new ProductCategoryController();
-    private SupplierController supplierController = new SupplierController();
-    private ShoppingCartController cartController = new ShoppingCartController();
+    private ProductController productController;
+    private ProductCategoryController categoryController;
+    private SupplierController supplierController;
+    private ShoppingCartController cartController;
 
     public ShopApp() {
         port(8888);
         staticFileLocation("/public");
 
-        post("basket/:id/add", cartController::handleAddToCartRequest, new ThymeleafTemplateEngine());
-
-        get("basket/:id/remove", cartController::handleRemoveFromCartRequest, new ThymeleafTemplateEngine());
-
-        get("basket/:id/edit", cartController::handleEditQuantityRequest, new ThymeleafTemplateEngine());
+        categoryController = new ProductCategoryController();
+        supplierController = new SupplierController();
+        cartController = new ShoppingCartController();
+        productController = new ProductController();
+//
+//        post("basket/:id/add", cartController::handleAddToCartRequest, new ThymeleafTemplateEngine());
+//
+//        get("basket/:id/remove", cartController::handleRemoveFromCartRequest, new ThymeleafTemplateEngine());
+//
+//        get("basket/:id/edit", cartController::handleEditQuantityRequest, new ThymeleafTemplateEngine());
 
         get("/category/:id", productController::renderAllProducts, new ThymeleafTemplateEngine());
 
         get("/supplier/:id", productController::renderAllProducts, new ThymeleafTemplateEngine());
-
-        get("basket/checkout", cartController::renderItemCheckout, new ThymeleafTemplateEngine());
-
-        post("basket/checkout", cartController::handleCheckoutRequest, new ThymeleafTemplateEngine());
-
-        get("/categories", categoryController::renderAllCategories, new ThymeleafTemplateEngine());
-
-        get("/suppliers", supplierController::renderAllSuppliers, new ThymeleafTemplateEngine());
-
-        get("/basket", cartController::renderCartItems, new ThymeleafTemplateEngine());
+//
+//        get("basket/checkout", cartController::renderItemCheckout, new ThymeleafTemplateEngine());
+//
+//        post("basket/checkout", cartController::handleCheckoutRequest, new ThymeleafTemplateEngine());
+//
+//        get("/categories", categoryController::renderAllCategories, new ThymeleafTemplateEngine());
+//
+//        get("/suppliers", supplierController::renderAllSuppliers, new ThymeleafTemplateEngine());
+//
+//        get("/basket", cartController::renderCartItems, new ThymeleafTemplateEngine());
 
         get("/", productController::renderAllProducts, new ThymeleafTemplateEngine());
+
     }
 }
