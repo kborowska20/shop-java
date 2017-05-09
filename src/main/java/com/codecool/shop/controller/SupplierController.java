a4df00a21e2de4b3ee6c10013a6d9f850ef5b1fc
Ickerday <elitarform@gmail.com>
2017-05-09 17:52:05 +0200

    Comment out deprecated methods

--- src/main/java/com/codecool/shop/controller/SupplierController.java
+++ src/main/java/com/codecool/shop/controller/SupplierController.java
@@ -1,8 +1,6 @@
 package com.codecool.shop.controller;
 
 import com.codecool.shop.dao.implementation.SupplierDaoSQLite;
-import com.codecool.shop.model.Supplier;
-import com.codecool.shop.view.SupplierView;
 import spark.ModelAndView;
 import spark.Request;
 import spark.Response;
@@ -19,15 +17,15 @@ public class SupplierController {
         return new ModelAndView(params, "supplier/index");
     }
 
-    public static void showSupplierBy(Integer id) {
-        SupplierView.printSupplier(supplierDao.find(id));
-    }
-
-    public static void addSupplier(String newSupplierName, String newSupplierDescription) {
-        supplierDao.add(new Supplier(newSupplierName, newSupplierDescription));
-    }
-
-    public static void removeSupplier(Integer id) {
-        supplierDao.remove(id);
-    }
+//    public static void showSupplierBy(Integer id) {
+//        SupplierView.printSupplier(supplierDao.find(id));
+//    }
+//
+//    public static void addSupplier(String newSupplierName, String newSupplierDescription) {
+//        supplierDao.add(new Supplier(newSupplierName, newSupplierDescription));
+//    }
+//
+//    public static void removeSupplier(Integer id) {
+//        supplierDao.remove(id);
+//    }
 }
