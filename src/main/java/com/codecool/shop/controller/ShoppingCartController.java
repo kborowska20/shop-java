5936e4f6d65d8f1aeb5bedc0a87ca3e7537991ab
Ickerday <elitarform@gmail.com>
2017-05-09 15:52:06 +0200

    Add renderCartItems() to ShoppingCartController, fix routes in Server

--- src/main/java/com/codecool/shop/controller/ShoppingCartController.java
+++ src/main/java/com/codecool/shop/controller/ShoppingCartController.java
@@ -1,80 +1,80 @@
 package com.codecool.shop.controller;
 
 import com.codecool.shop.dao.implementation.ProductDaoSQLite;
-import com.codecool.shop.model.CartItem;
-import com.codecool.shop.model.Product;
 import com.codecool.shop.model.ShoppingCart;
-import com.codecool.shop.view.MenuView;
-import com.codecool.shop.view.ShoppingCartView;
+import spark.ModelAndView;
+import spark.Request;
+import spark.Response;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class ShoppingCartController {
     private static ProductDaoSQLite productDao = new ProductDaoSQLite();
+    private ShoppingCart shoppingCart = new ShoppingCart();
 
-    public static void renderCartItems(ShoppingCart shoppingCart) {
-        ShoppingCartView.printCartItemList(shoppingCart.getItemList());
+    public ModelAndView renderCartItems(Request req, Response res) {
+        Map<String, Object> params = new HashMap<>();
+        params.put("cartItemList", shoppingCart.getItemList());
+
+        return new ModelAndView(params, "basket/index");
     }
 
-    public static void handleAddToCartRequest(ShoppingCart shoppingCart) {
-        MenuController.showMessage("Please select ID of the product:");
-        Integer productIdInput = InputCollector.getNextInt();
-
-        MenuController.showMessage("Please input quantity of the product (between 1 and 250)");
-        Integer productQuantityInput = InputCollector.getNextInt();
-
-        Product addedProduct = productDao.find(productIdInput);
-        if (addedProduct != null && productQuantityInput > 0 && productQuantityInput < 250) {
-            shoppingCart.addProduct(new CartItem(addedProduct, productQuantityInput));
-        } else {
-            throw new ArrayIndexOutOfBoundsException();
-        }
-    }
-
-    public static void handleRemoveFromCartRequest(ShoppingCart shoppingCart) {
-        ShoppingCartController.renderCartItems(shoppingCart);
-        MenuController.showMessage("Please select ID of the item:");
-        Integer itemIdInput = InputCollector.getNextInt();
-
-        shoppingCart.removeProduct(getItemBy(shoppingCart, itemIdInput));
-    }
-
-    private static CartItem getItemBy(ShoppingCart shoppingCart, Integer id) {
-        CartItem foundCartItem = null;
-        for (CartItem cartItem : shoppingCart.getItemList()) {
-            if (cartItem.getId().equals(id)) {
-                foundCartItem = cartItem;
-                break;
-            }
-        }
-        return foundCartItem;
-    }
-
-    public static void handleEditQuantityRequest(ShoppingCart shoppingCart) {
-        ShoppingCartController.renderCartItems(shoppingCart);
-        MenuController.showMessage("Please select ID of the item:");
-        Integer cartItemId = InputCollector.getNextInt();
-
-        MenuController.showMessage("Please input new quantity:");
-        Integer newItemQuantity = InputCollector.getNextInt();
-
-
-        if (newItemQuantity > 0 && newItemQuantity < 250) {
-            getItemBy(shoppingCart, cartItemId).setProductQuantity(newItemQuantity);
-        } else {
-            throw new ArithmeticException("Too many items requested!");
-        }
-    }
-
-
-    public static void handleCheckoutRequest(ShoppingCart shoppingCart) {
-        MenuView.printMessage("You're about to checkout items for a total of: " +
-                shoppingCart.calculateCheckoutPrice());
-
-        MenuView.printMessage("Please enter your full name:");
-        InputCollector.getNext();
-        MenuView.printMessage("Please enter your city:");
-        InputCollector.getNext();
-
-        shoppingCart.getItemList().clear();
-        MenuView.printMessage("Transaction confirmed! Thank you for buying from our shop.");
-    }
+//    public static void handleAddToCartRequest(ShoppingCart shoppingCart) {
+//        Product addedProduct = productDao.find(productIdInput);
+//        if (addedProduct != null && req.queryParams() > 0 && req.queryParams < 250) {
+//            shoppingCart.addProduct(new CartItem(addedProduct, productQuantityInput));
+//        } else {
+//            throw new ArrayIndexOutOfBoundsException();
+//        }
+//    }
+//
+//    public static void handleRemoveFromCartRequest(ShoppingCart shoppingCart) {
+//        ShoppingCartController.renderCartItems(shoppingCart);
+//        MenuController.showMessage("Please select ID of the item:");
+//        Integer itemIdInput = InputCollector.getNextInt();
+//
+//        shoppingCart.removeProduct(getItemBy(shoppingCart, itemIdInput));
+//    }
+//
+//    private static CartItem getItemBy(ShoppingCart shoppingCart, Integer id) {
+//        CartItem foundCartItem = null;
+//        for (CartItem cartItem : shoppingCart.getItemList()) {
+//            if (cartItem.getId().equals(id)) {
+//                foundCartItem = cartItem;
+//                break;
+//            }
+//        }
+//        return foundCartItem;
+//    }
+//
+//    public static void handleEditQuantityRequest(ShoppingCart shoppingCart) {
+//        ShoppingCartController.renderCartItems(shoppingCart);
+//        MenuController.showMessage("Please select ID of the item:");
+//        Integer cartItemId = InputCollector.getNextInt();
+//
+//        MenuController.showMessage("Please input new quantity:");
+//        Integer newItemQuantity = InputCollector.getNextInt();
+//
+//
+//        if (newItemQuantity > 0 && newItemQuantity < 250) {
+//            getItemBy(shoppingCart, cartItemId).setProductQuantity(newItemQuantity);
+//        } else {
+//            throw new ArithmeticException("Too many items requested!");
+//        }
+//    }
+//
+//
+//    public static void handleCheckoutRequest(ShoppingCart shoppingCart) {
+//        MenuView.printMessage("You're about to checkout items for a total of: " +
+//                shoppingCart.calculateCheckoutPrice());
+//
+//        MenuView.printMessage("Please enter your full name:");
+//        InputCollector.getNext();
+//        MenuView.printMessage("Please enter your city:");
+//        InputCollector.getNext();
+//
+//        shoppingCart.getItemList().clear();
+//        MenuView.printMessage("Transaction confirmed! Thank you for buying from our shop.");
+//    }
 }
