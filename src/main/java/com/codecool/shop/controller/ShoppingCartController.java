419d55e54c476dc1a239bf90df37bd1a64366ac6
Ickerday <elitarform@gmail.com>
2017-05-09 17:52:52 +0200

    Add editQuantity, addToCart, getItemBy, checkoutRequest

--- src/main/java/com/codecool/shop/controller/ShoppingCartController.java
+++ src/main/java/com/codecool/shop/controller/ShoppingCartController.java
@@ -1,6 +1,9 @@
 package com.codecool.shop.controller;
 
+import com.codecool.shop.dao.ProductDao;
 import com.codecool.shop.dao.implementation.ProductDaoSQLite;
+import com.codecool.shop.model.CartItem;
+import com.codecool.shop.model.Product;
 import com.codecool.shop.model.ShoppingCart;
 import spark.ModelAndView;
 import spark.Request;
@@ -10,7 +13,8 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class ShoppingCartController {
-    private static ProductDaoSQLite productDao = new ProductDaoSQLite();
+    private ProductController productController = new ProductController();
+    private ProductDao productDao = new ProductDaoSQLite();
     private ShoppingCart shoppingCart = new ShoppingCart();
 
     public ModelAndView renderCartItems(Request req, Response res) {
@@ -20,61 +24,66 @@ public class ShoppingCartController {
         return new ModelAndView(params, "basket/index");
     }
 
-//    public static void handleAddToCartRequest(ShoppingCart shoppingCart) {
-//        Product addedProduct = productDao.find(productIdInput);
-//        if (addedProduct != null && req.queryParams() > 0 && req.queryParams < 250) {
-//            shoppingCart.addProduct(new CartItem(addedProduct, productQuantityInput));
-//        } else {
-//            throw new ArrayIndexOutOfBoundsException();
-//        }
-//    }
-//
-//    public static void handleRemoveFromCartRequest(ShoppingCart shoppingCart) {
-//        ShoppingCartController.renderCartItems(shoppingCart);
-//        MenuController.showMessage("Please select ID of the item:");
-//        Integer itemIdInput = InputCollector.getNextInt();
-//
-//        shoppingCart.removeProduct(getItemBy(shoppingCart, itemIdInput));
-//    }
-//
-//    private static CartItem getItemBy(ShoppingCart shoppingCart, Integer id) {
-//        CartItem foundCartItem = null;
-//        for (CartItem cartItem : shoppingCart.getItemList()) {
-//            if (cartItem.getId().equals(id)) {
-//                foundCartItem = cartItem;
-//                break;
-//            }
-//        }
-//        return foundCartItem;
-//    }
-//
-//    public static void handleEditQuantityRequest(ShoppingCart shoppingCart) {
-//        ShoppingCartController.renderCartItems(shoppingCart);
-//        MenuController.showMessage("Please select ID of the item:");
-//        Integer cartItemId = InputCollector.getNextInt();
-//
-//        MenuController.showMessage("Please input new quantity:");
-//        Integer newItemQuantity = InputCollector.getNextInt();
-//
-//
-//        if (newItemQuantity > 0 && newItemQuantity < 250) {
-//            getItemBy(shoppingCart, cartItemId).setProductQuantity(newItemQuantity);
-//        } else {
-//            throw new ArithmeticException("Too many items requested!");
-//        }
-//    }
-//
-//
-//    public static void handleCheckoutRequest(ShoppingCart shoppingCart) {
-//        MenuView.printMessage("You're about to checkout items for a total of: " +
-//                shoppingCart.calculateCheckoutPrice());
-//
-//        MenuView.printMessage("Please enter your full name:");
-//        InputCollector.getNext();
-//        MenuView.printMessage("Please enter your city:");
-//        InputCollector.getNext();
-//
-//        shoppingCart.getItemList().clear();
-//        MenuView.printMessage("Transaction confirmed! Thank you for buying from our shop.");
-//    }
+    public ModelAndView handleAddToCartRequest(Request req, Response res) {
+        Integer productID = Integer.parseInt(req.params().get(":pid"));
+
+        if (!(productID == null) || !productID.equals(0)) {
+            Product addedProduct = productDao.find(productID);
+            shoppingCart.addProduct(new CartItem(addedProduct, 1));
+
+            res.status(201);
+        } else {
+            res.status(400);
+        }
+        res.redirect("/");
+        return productController.renderProducts(req, res);
+    }
+
+    public ModelAndView handleRemoveFromCartRequest(Request req, Response res) {
+        Integer itemID = Integer.parseInt(req.params().get(":pid"));
+
+        if (!(itemID == null) || !itemID.equals(0)) {
+            shoppingCart.removeProduct(getItemBy(itemID));
+            res.status(201);
+        } else {
+            res.status(400);
+        }
+        res.redirect("/");
+        return productController.renderProducts(req, res);
+    }
+
+    private CartItem getItemBy(Integer id) {
+        CartItem foundCartItem = null;
+        for (CartItem cartItem : shoppingCart.getItemList()) {
+            if (cartItem.getId().equals(id)) {
+                foundCartItem = cartItem;
+                break;
+            }
+        }
+        return foundCartItem;
+    }
+
+    public ModelAndView handleEditQuantityRequest(Request req, Response res) {
+        Integer itemID = Integer.parseInt(req.params().get(":pid"));
+        Integer newItemQuantity = Integer.parseInt(req.params().get("item-quantity"));
+
+        if (!(itemID == null) && newItemQuantity > 0 && newItemQuantity < 250) {
+            getItemBy(itemID).setProductQuantity(newItemQuantity);
+
+            res.status(201);
+        } else {
+            res.status(400);
+        }
+        res.redirect("/basket");
+        return productController.renderProducts(req, res);
+    }
+
+
+    public ModelAndView handleCheckoutRequest(Request req, Response res) {
+        shoppingCart.getItemList().clear();
+
+        res.redirect("/", 201);
+        return productController.renderProducts(req, res);
+
+    }
 }
