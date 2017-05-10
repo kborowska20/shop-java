378d30d5ae377c50a6375f1f81781bffa31eabb1
Sebastian Znaj <seb.znaj@gmail.com>
2017-05-10 15:22:31 +0200

    Update renderAllCategory() to return ModelAndView

--- src/main/java/com/codecool/shop/controller/ProductCategoryController.java
+++ src/main/java/com/codecool/shop/controller/ProductCategoryController.java
@@ -3,12 +3,20 @@ package com.codecool.shop.controller;
 import com.codecool.shop.dao.implementation.ProductCategoryDaoSQLite;
 import com.codecool.shop.model.ProductCategory;
 import com.codecool.shop.view.ProductCategoryView;
+import spark.ModelAndView;
+import spark.Request;
+import spark.Response;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class ProductCategoryController {
     private static ProductCategoryDaoSQLite categoryDao = new ProductCategoryDaoSQLite();
 
-    public static void renderAllCategories() {
-        ProductCategoryView.printCategoryList(categoryDao.getAll());
+    public ModelAndView renderAllCategories(Request req, Response res) {
+        Map<String, Object> params = new HashMap<>();
+        params.put("categoryList", categoryDao.getAll());
+        return new ModelAndView(params, "category/index");
     }
 
     public static void showCategoryBy(Integer id) {
