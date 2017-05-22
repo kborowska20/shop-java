b93ac728814e8ab1d6419e620b7a10b60d71d64e
Morthan666 <m.k.gozdzikiewicz@gmail.com>
2017-05-22 14:30:28 +0200

    Add getDefaultPrice test

--- src/main/test/com/codecool/shop/model/ProductTest.java
+++ src/main/test/com/codecool/shop/model/ProductTest.java
@@ -12,8 +12,22 @@ class ProductTest {
     void testToString() {
         Supplier supplier = new Supplier("TestSupplier", "Testscription");
         ProductCategory productCategory = new ProductCategory("TestCategory");
-        Product product = new Product("TestProduct", 1.5f, "PLN", "TestDescription", productCategory, supplier);
-        assertEquals("id: 0, name: TestProduct, defaultPrice: 1,50, defaultCurrency: PLN, productCategory: TestCategory, supplier: TestSupplier", product.toString());
+        Product product = new Product("TestProduct", 1.5f, "PLN",
+                "TestDescription", productCategory, supplier);
+        assertEquals("id: 0, name: TestProduct, defaultPrice: 1,50, defaultCurrency: PLN," +
+                " productCategory: TestCategory, supplier: TestSupplier", product.toString());
     }
+
+    @Test
+    void testGetDefaultPrice() {
+        Supplier supplier = new Supplier("TestSupplier", "Testscription");
+        ProductCategory productCategory = new ProductCategory("TestCategory");
+        Product product = new Product("TestProduct", 0, "PLN",
+                "TestDescription", productCategory, supplier);
+        product.setDefaultPrice(1.5f);
+        assertEquals(1.5f, product.getDefaultPrice());
+    }
+
+
 
 }
