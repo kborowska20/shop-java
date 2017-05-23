960ccb16095d927a45592311c6aeba9e9b5c483e
oskardz <oskardz1991@gmail.com>
2017-05-23 11:29:53 +0200

    Add Product Category tests, Coverage 100%

---
+++ src/main/test/com/codecool/shop/controller/ProductCategoryControllerTest.java
@@ -0,0 +1,23 @@
+package com.codecool.shop.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Created by oskar on 22.05.17.
+ */
+class ProductCategoryControllerTest {
+    @Test
+    void renderAllCategories() {
+    }
+
+    @Test
+    void addCategory() {
+    }
+
+    @Test
+    void removeCategory() {
+    }
+
+}
