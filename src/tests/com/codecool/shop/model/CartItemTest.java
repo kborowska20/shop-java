8967468103e9c1061f01656b9e16140191db7986
klaudia borowska <canimi18@gmial.com>
2017-05-23 10:56:25 +0200

    Create Get TotalPrice test

--- src/tests/com/codecool/shop/model/CartItemTest.java
+++ src/tests/com/codecool/shop/model/CartItemTest.java
@@ -1,10 +1,31 @@
 package com.codecool.shop.model;
 
+import com.codecool.shop.dao.implementation.DbConnector;
+import com.codecool.shop.dao.implementation.ProductDaoSQLite;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
-/**
- * Created by KBorowska on 23.05.2017.
- */
 class CartItemTest {
+
+    @Test
+    public void TestGetTotalPrice(){
+        DbConnector dbConnector = new DbConnector();
+        ProductDaoSQLite productDaoSQLite = new ProductDaoSQLite(dbConnector.getConnection());
+        Product product = productDaoSQLite.find(2);
+        CartItem cartItem = new CartItem(product,2);
+        Float totalPrice = cartItem.getTotalPrice();
+        Float realPrice = 3.4f;
+        assertEquals(totalPrice,realPrice);
+    }
+    @Test
+    public void TestGetProduct(){
+        DbConnector dbConnector = new DbConnector();
+        ProductDaoSQLite productDaoSQLite = new ProductDaoSQLite(dbConnector.getConnection());
+        Product product = productDaoSQLite.find(2);
+        CartItem cartItem = new CartItem(product,2);
+        Product productCartItem = cartItem.getProduct();
+        assertEquals(product,productCartItem);
+    }
 
 }
