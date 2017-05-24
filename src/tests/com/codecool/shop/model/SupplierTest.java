46dc566199f9f56b44b0365e075241cb90504de7
klaudia borowska <canimi18@gmial.com>
2017-05-24 14:56:33 +0200

    Add testAddProduct test

--- src/tests/com/codecool/shop/model/SupplierTest.java
+++ src/tests/com/codecool/shop/model/SupplierTest.java
@@ -23,6 +23,15 @@ class SupplierTest {
         supplier.setProducts(testProducts);
         assertEquals(testProducts, supplier.getProducts());
     }
+    @org.junit.jupiter.api.Test
+    void testAddProduct() {
+        ArrayList<Product> testProducts= new ArrayList<>();
+        Product product= new Product(1, null, 100,"PLN", null, null, null);
+        testProducts.add(product);
+        Supplier supplier = new Supplier("SupplierName","supplierDisc");
+        supplier.addProduct(product);
+        assertEquals(testProducts, supplier.getProducts());
+    }
 
 
 }
