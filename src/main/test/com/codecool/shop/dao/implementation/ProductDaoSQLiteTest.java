20771996850169fb9d0737b0ee746d15a169d422
Morthan666 <m.k.gozdzikiewicz@gmail.com>
2017-05-24 10:29:25 +0200

    Add getByCategory test

--- src/main/test/com/codecool/shop/dao/implementation/ProductDaoSQLiteTest.java
+++ src/main/test/com/codecool/shop/dao/implementation/ProductDaoSQLiteTest.java
@@ -1,8 +1,11 @@
 package com.codecool.shop.dao.implementation;
 
+import com.codecool.shop.model.ProductCategory;
 import org.junit.jupiter.api.Test;
 
 import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -11,15 +14,26 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
  */
 class ProductDaoSQLiteTest {
 
+
     @Test
-    void testFindProduct() {
-        DbConnector dbConnector = new DbConnector();
-        Connection conn = dbConnector.getConnection();
+    void testFindProduct() throws SQLException {
+        Connection conn = DriverManager.getConnection("jdbc:sqlite:src/main/test/resources/test.db");
         ProductDaoSQLite productDaoSQLite = new ProductDaoSQLite(conn);
-//        ProductCategory productCategory = new ProductCategory("Dairy", "test","test");
-//        Supplier supplier= new Supplier("Mlekpol", "test");
-//        Product product =  new Product(2,"Milk", 1.7f, "PLN", "testcription", productCategory, supplier);
-        assertEquals("id: 2, name: Milk, defaultPrice: 1,70, defaultCurrency: PLN, productCategory: Dairy, supplier: Mlekpol", productDaoSQLite.find(2).toString());
+        assertEquals("id: 2, name: Milk, defaultPrice: 1,70, defaultCurrency: PLN," +
+                " productCategory: Dairy, supplier: Mlekpol", productDaoSQLite.find(2).toString());
+    }
+
+    @Test
+    void testGetByCategory() throws SQLException {
+        Connection conn = DriverManager.getConnection("jdbc:sqlite:src/main/test/resources/test.db");
+        ProductDaoSQLite productDaoSQLite = new ProductDaoSQLite(conn);
+        ProductCategory productCategory = new ProductCategory(1, "Test", "Test", "Test");
+        assertEquals("[id: 3, name: Bananas, defaultPrice: 3,10, defaultCurrency: PLN, productCategory: Test," +
+                        " supplier: Boongaboonga, id: 6, name: Apples, defaultPrice: 1,40, defaultCurrency: PLN," +
+                        " productCategory: Test, supplier: PolSad, id: 9, name: Tomatoes, defaultPrice: 3,70, " +
+                        "defaultCurrency: PLN, productCategory: Test, supplier: PolSad]",
+                productDaoSQLite.getBy(productCategory).toString());
+
     }
 
 
