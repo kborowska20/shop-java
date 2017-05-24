19137d35328d6c4dc05d08b1cc88ee8606687425
klaudia borowska <canimi18@gmial.com>
2017-05-24 11:41:12 +0200

    Add testChaneQuanitityMethod test

--- src/tests/com/codecool/shop/controller/ShoppingCartControllerTest.java
+++ src/tests/com/codecool/shop/controller/ShoppingCartControllerTest.java
@@ -25,7 +25,7 @@ import static org.junit.jupiter.api.Assertions.*;
 class ShoppingCartControllerTest {
 
     @Test
-    public void testRenderCartItemsMethod() throws SQLException {
+    public void testAddCartItemsMethod() throws SQLException {
         Request req = mock(Request.class);
         Response res = mock(Response.class);
         HashMap<String, String> hashMap = new HashMap<String,String>();
@@ -42,5 +42,40 @@ class ShoppingCartControllerTest {
                 "supplierList=[id: 1, name: Mlekpol, description: Polish dairy products., " +
                 "id: 2, name: Sokołów, description: Our sausages are actually superior., id: 3, name: Felix, description: Suck on that peanut, won't ya'?, " +
                 "id: 4, name: Boongaboonga, description: Our food isn't safe to eat., id: 5, name: PolSad, description: We only sell apples., id: 6, name: Piekarnia Mojego Taty, description: Actually, only Mom knows how to make the bread we sell.]}", shoppingCartModelAndView.getModel().toString());
+    }
+    @Test
+    public void testRemoveFromCartRequest() throws SQLException {
+        Request req = mock(Request.class);
+        Response res = mock(Response.class);
+        HashMap<String, String> hashMap = new HashMap<String,String>();
+        hashMap.put(":pid","2");
+        when(req.params()).thenReturn(hashMap);
+        Connection conn = null;
+        conn = DriverManager.getConnection("jdbc:sqlite:src/tests/com/codecool/shop/resources/db/Products_test.db");
+        ProductController productController = new ProductController(conn);
+        ShoppingCartController shoppingCartController = new ShoppingCartController(conn, productController);
+        shoppingCartController.handleAddToCartRequest(req,res);
+        shoppingCartController.handleRemoveFromCartRequest(req,res);
+        ModelAndView shoppingCartModelAndView = shoppingCartController.renderCartItems(req, res);
+        assertEquals("",shoppingCartModelAndView.getModel().toString());
+    }
+    @Test
+    public void testHandleEditQuantityRequest() throws SQLException {
+        Request req = mock(Request.class);
+        Response res = mock(Response.class);
+        HashMap<String, String> hashMap = new HashMap<String,String>();
+        hashMap.put(":pid","2");
+        hashMap.put("item-quantity","5");
+        when(req.params()).thenReturn(hashMap);
+        Connection conn = null;
+        conn = DriverManager.getConnection("jdbc:sqlite:src/tests/com/codecool/shop/resources/db/Products_test.db");
+        ProductController productController = new ProductController(conn);
+        ShoppingCartController shoppingCartController = new ShoppingCartController(conn, productController);
+        shoppingCartController.handleAddToCartRequest(req,res);
+        shoppingCartController.handleEditQuantityRequest(req,res);
+        ModelAndView shoppingCartModelAndView = shoppingCartController.renderCartItems(req, res);
+        assertEquals("{cartItemList=" +
+                "[CartItem{id=2, product=id: 2, name: Milk, defaultPrice: 1,70, defaultCurrency: PLN, productCategory: Dairy, supplier: Mlekpol, productQuantity=5, totalPrice=8.5}]," +
+                " supplierList=[id: 1, name: Mlekpol, description: Polish dairy products., id: 2, name: Sokołów, description: Our sausages are actually superior., id: 3, name: Felix, description: Suck on that peanut, won't ya'?, id: 4, name: Boongaboonga, description: Our food isn't safe to eat., id: 5, name: PolSad, description: We only sell apples., id: 6, name: Piekarnia Mojego Taty, description: Actually, only Mom knows how to make the bread we sell.]}",shoppingCartModelAndView.getModel().toString());
     }
 }
