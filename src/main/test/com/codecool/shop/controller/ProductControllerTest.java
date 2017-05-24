08dbfa52bfd877b6e425e8dad674cc6b0576897a
Morthan666 <m.k.gozdzikiewicz@gmail.com>
2017-05-24 12:05:58 +0200

    Add renderProductsByCategory test

--- src/main/test/com/codecool/shop/controller/ProductControllerTest.java
+++ src/main/test/com/codecool/shop/controller/ProductControllerTest.java
@@ -76,5 +76,29 @@ class ProductControllerTest {
                 , productController.renderProducts(req, res).getModel().toString());
     }
 
-    
+    @Test
+    void renderProductsByCategory() throws SQLException {
+        Request req = mock(Request.class);
+        Response res = mock(Response.class);
+        Connection conn = DriverManager.getConnection("jdbc:sqlite:src/main/test/resources/test.db");
+        ProductController productController = new ProductController(conn);
+        HashMap<String, String> hashMap = new HashMap<String, String>();
+        hashMap.put(":cid", "2");
+        when(req.params()).thenReturn(hashMap);
+        assertEquals("{categoryList=[id: 1, name: Fruit, department: Food, description: The best the Earth has given us.," +
+                        " id: 2, name: Dairy, department: Food, description: Stolen from under a cow's nose.," +
+                        " id: 3, name: Pastries, department: Food, description: Tasty and fresh., " +
+                        "id: 4, name: Meat, department: Food, description: Always fresh., id: 5, name: Processed," +
+                        " department: Food, description: Forever fresh.], supplierList=[id: 1, name: Mlekpol, " +
+                        "description: Polish dairy products., id: 2, name: Sokołów, description: Our sausages are actually superior.," +
+                        " id: 3, name: Felix, description: Suck on that peanut, won't ya'?, id: 4, name: Boongaboonga," +
+                        " description: Our food isn't safe to eat., id: 5, name: PolSad, description: We only sell apples.," +
+                        " id: 6, name: Piekarnia Mojego Taty, description: Actually, only Mom knows how to make the bread we sell.]," +
+                        " productList=[id: 2, name: Milk, defaultPrice: 1,70, defaultCurrency: PLN, productCategory: Dairy," +
+                        " supplier: Mlekpol, id: 4, name: Butter, defaultPrice: 4,20, defaultCurrency: PLN," +
+                        " productCategory: Dairy, supplier: Mlekpol, id: 7, name: White cheese, defaultPrice: 1,80," +
+                        " defaultCurrency: PLN, productCategory: Dairy, supplier: Mlekpol, id: 8, name: Eggs, " +
+                        "defaultPrice: 5,40, defaultCurrency: PLN, productCategory: Dairy, supplier: Felix]}"
+                , productController.renderProducts(req, res).getModel().toString());
+    }
 }
