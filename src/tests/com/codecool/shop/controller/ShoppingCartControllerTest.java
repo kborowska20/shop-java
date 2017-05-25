f4c312986dcc6aa056486bf29ab3d4674546ba17
Morthan666 <m.k.gozdzikiewicz@gmail.com>
2017-05-25 18:27:52 +0200

    Insert blank line

--- src/tests/com/codecool/shop/controller/ShoppingCartControllerTest.java
+++ src/tests/com/codecool/shop/controller/ShoppingCartControllerTest.java
@@ -36,6 +36,7 @@ class ShoppingCartControllerTest {
         Integer intCat = arr.get(0).getId();
         assertEquals("{cartItemList=[CartItem{id=" + intCat + ", product=id: 2, name: Milk, defaultPrice: 1,70, defaultCurrency: PLN, productCategory: Dairy, supplier: Mlekpol, productQuantity=1, totalPrice=1.7}], supplierList=[id: 1, name: Mlekpol, description: Polish dairy products., id: 2, name: Sokołów, description: Our sausages are actually superior., id: 3, name: Felix, description: Suck on that peanut, won't ya'?, id: 4, name: Boongaboonga, description: Our food isn't safe to eat., id: 5, name: PolSad, description: We only sell apples., id: 6, name: Piekarnia Mojego Taty, description: Actually, only Mom knows how to make the bread we sell.]}", shoppingCartModelAndView.getModel().toString());
     }
+
     @Test
     public void testRemoveFromCartRequest() throws SQLException {
         Request req = mock(Request.class);
