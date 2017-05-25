a5452c85786dce7f3d3694a0159b615fe9c70a4b
klaudia borowska <canimi18@gmial.com>
2017-05-25 13:44:29 +0200

    Change Product obj in CartIteamTest

--- src/tests/com/codecool/shop/model/CartItemTest.java
+++ src/tests/com/codecool/shop/model/CartItemTest.java
@@ -10,86 +10,101 @@ class CartItemTest {
 
     @Test
     public void TestGetTotalPrice(){
-        DbConnector dbConnector = new DbConnector();
-        ProductDaoSQLite productDaoSQLite = new ProductDaoSQLite(dbConnector.getConnection());
-        Product product = productDaoSQLite.find(2);
-        CartItem cartItem = new CartItem(product,2);
+        Supplier supplier = new Supplier("TestSupplier", "Testscription");
+        ProductCategory productCategory = new ProductCategory("TestCategory");
+        Product product = new Product("TestProduct", 1.7f, "PLN",
+                "TestDescription", productCategory, supplier);        CartItem cartItem = new CartItem(product,2);
         Float totalPrice = cartItem.getTotalPrice();
         Float realPrice = 3.4f;
         assertEquals(totalPrice,realPrice);
     }
+
     @Test
     public void TestGetProduct(){
-        DbConnector dbConnector = new DbConnector();
-        ProductDaoSQLite productDaoSQLite = new ProductDaoSQLite(dbConnector.getConnection());
-        Product product = productDaoSQLite.find(2);
+        Supplier supplier = new Supplier("TestSupplier", "Testscription");
+        ProductCategory productCategory = new ProductCategory("TestCategory");
+        Product product = new Product("TestProduct", 1.7f, "PLN",
+                "TestDescription", productCategory, supplier);
         CartItem cartItem = new CartItem(product,2);
         Product productCartItem = cartItem.getProduct();
         assertEquals(product,productCartItem);
     }
+
     @Test
     public void TestGetId(){
-        DbConnector dbConnector = new DbConnector();
-        ProductDaoSQLite productDaoSQLite = new ProductDaoSQLite(dbConnector.getConnection());
-        Product product = productDaoSQLite.find(2);
-        Product product1 = productDaoSQLite.find(1);
+        Supplier supplier = new Supplier("TestSupplier", "Testscription");
+        ProductCategory productCategory = new ProductCategory("TestCategory");
+        Product product = new Product("TestProduct", 1.5f, "PLN",
+                "TestDescription", productCategory, supplier);
+        Product product1 = new Product("TestProduct", 1.5f, "PLN",
+                "TestDescription", productCategory, supplier);
         CartItem cartItem = new CartItem(product,2);
         CartItem cartItem1 = new CartItem(product1,1);
         Integer cartItemId = cartItem.getId();
         Integer cartItemId1 = cartItem1.getId();
         assertTrue(cartItemId < cartItemId1);
     }
+
     @Test
     public void TestGetProductQuantity(){
-        DbConnector dbConnector = new DbConnector();
-        ProductDaoSQLite productDaoSQLite = new ProductDaoSQLite(dbConnector.getConnection());
-        Product product = productDaoSQLite.find(2);
+        Supplier supplier = new Supplier("TestSupplier", "Testscription");
+        ProductCategory productCategory = new ProductCategory("TestCategory");
+        Product product = new Product("TestProduct", 1.7f, "PLN",
+                "TestDescription", productCategory, supplier);
         CartItem cartItem = new CartItem(product,2);
         Integer cartIteamQuantity = cartItem.getProductQuantity();
         Integer quantity = 2;
         assertEquals(quantity,cartIteamQuantity);
     }
+
     @Test
     public void TestIsAddToProductQuantityIsCounting(){
-        DbConnector dbConnector = new DbConnector();
-        ProductDaoSQLite productDaoSQLite = new ProductDaoSQLite(dbConnector.getConnection());
-        Product product = productDaoSQLite.find(2);
+        Supplier supplier = new Supplier("TestSupplier", "Testscription");
+        ProductCategory productCategory = new ProductCategory("TestCategory");
+        Product product = new Product("TestProduct", 1.5f, "PLN",
+                "TestDescription", productCategory, supplier);
         CartItem cartItem = new CartItem(product,2);
         cartItem.addToProductQuantity(5);
         Integer cartIteamQuantity = cartItem.getProductQuantity();
         Integer quantity = 7;
         assertEquals(quantity,cartIteamQuantity);
     }
+
     @Test
     public void TestIsGetDefReturn(){
-        DbConnector dbConnector = new DbConnector();
-        ProductDaoSQLite productDaoSQLite = new ProductDaoSQLite(dbConnector.getConnection());
-        Product product = productDaoSQLite.find(2);
+        Supplier supplier = new Supplier("TestSupplier", "Testscription");
+        ProductCategory productCategory = new ProductCategory("TestCategory");
+        Product product = new Product("TestProduct", 1.5f, "PLN",
+                "TestDescription", productCategory, supplier);
         CartItem cartItem = new CartItem(product,2);
         cartItem.setProductQuantity(5);
         Integer cartIteamQuantity = cartItem.getProductQuantity();
         Integer quantity = 5;
         assertEquals(quantity,cartIteamQuantity);
     }
+
     @Test
     public void TestIsToString(){
-        DbConnector dbConnector = new DbConnector();
-        ProductDaoSQLite productDaoSQLite = new ProductDaoSQLite(dbConnector.getConnection());
-        Product product = productDaoSQLite.find(2);
+        Supplier supplier = new Supplier("TestSupplier", "Testscription");
+        ProductCategory productCategory = new ProductCategory("TestCategory");
+        Product product = new Product(2,"TestProduct", 1.7f, "PLN",
+                "TestDescription", productCategory, supplier);
         CartItem cartItem = new CartItem(product,2);
         String cartItemToString = cartItem.toString();
-        String quantity = "CartItem{id=11, product=id: 2, name: Milk, defaultPrice: 1,70, defaultCurrency: PLN, productCategory: Dairy, supplier: Mlekpol, productQuantity=2, totalPrice=3.4}";
+        String quantity = "CartItem{id=0, product=id: 2, name: TestProduct, defaultPrice: 1,70, defaultCurrency: PLN, productCategory: TestCategory, supplier: TestSupplier, productQuantity=2, totalPrice=3.4}";
         assertEquals(quantity,cartItemToString);
     }
+
     @Test
     public void TestIsSetChangeQuantity(){
-        DbConnector dbConnector = new DbConnector();
-        ProductDaoSQLite productDaoSQLite = new ProductDaoSQLite(dbConnector.getConnection());
-        Product product = productDaoSQLite.find(2);
+        Supplier supplier = new Supplier("TestSupplier", "Testscription");
+        ProductCategory productCategory = new ProductCategory("TestCategory");
+        Product product = new Product(2,"TestProduct", 1.7f, "PLN",
+                "TestDescription", productCategory, supplier);
         CartItem cartItem = new CartItem(product,2);
         cartItem.setProductQuantity(6);
         String cartItemToString = cartItem.toString();
-        String quantity = "CartItem{id=6, product=id: 2, name: Milk, defaultPrice: 1,70, defaultCurrency: PLN, productCategory: Dairy, supplier: Mlekpol, productQuantity=6, totalPrice=10,2}";
+        String quantity = "CartItem{id=0, product=id: 2, name: TestProduct, defaultPrice: 1,70, defaultCurrency: PLN, productCategory: TestCategory, supplier: TestSupplier, productQuantity=6, totalPrice=10.2}";
         assertEquals(quantity,cartItemToString);
     }
 
